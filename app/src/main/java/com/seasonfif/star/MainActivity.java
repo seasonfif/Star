7dc5c680487739d58f8185cc99dfc196410b1e6e
第五季 <seasonfif@live.com>
2018-01-26 18:39:33 +0800

    init

---
+++ app/src/main/java/com/seasonfif/star/MainActivity.java
@@ -0,0 +1,56 @@
+package com.seasonfif.star;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import com.seasonfif.star.view.BottomNavigationViewHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+  private TextView mTextMessage;
+
+  private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
+      new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+          Intent intent = new Intent();
+          switch (item.getItemId()) {
+            case R.id.navigation_home:
+              mTextMessage.setText(R.string.title_home);
+              return true;
+            case R.id.navigation_info:
+              mTextMessage.setText(R.string.title_info);
+              return true;
+            case R.id.navigation_dashboard:
+              mTextMessage.setText(R.string.title_dashboard);
+              intent.setClass(MainActivity.this, SettingsActivity.class);
+              startActivity(intent);
+              return true;
+            case R.id.navigation_notifications:
+              mTextMessage.setText(R.string.title_notifications);
+              return true;
+          }
+
+          return false;
+        }
+      };
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mTextMessage = (TextView) findViewById(R.id.message);
+    BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+    navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    BottomNavigationViewHelper.disableShiftMode(navigation);
+  }
+}
+
+
