d9c0f7d32d0b43f34d2dc70c13c6a9b7142d6f98
第五季 <seasonfif@live.com>
2018-02-05 17:40:50 +0800

    删除冗余代码

--- app/src/main/java/com/seasonfif/star/ui/activity/SplashActivity.java
+++ app/src/main/java/com/seasonfif/star/ui/activity/SplashActivity.java
@@ -1,18 +1,15 @@
 package com.seasonfif.star.ui.activity;
 
 import android.content.Intent;
-import android.graphics.drawable.Animatable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.graphics.drawable.Animatable2Compat;
-import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
-import android.support.v7.widget.AppCompatImageView;
-
-import com.seasonfif.star.R;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.seasonfif.star.R;
+import com.seasonfif.star.widget.titanic.Titanic;
+import com.seasonfif.star.widget.titanic.TitanicTextView;
+import com.seasonfif.star.widget.titanic.Typefaces;
 
 /**
  * Created by seasonfif on 2018/2/4.
@@ -20,8 +17,7 @@ import butterknife.ButterKnife;
 
 public class SplashActivity extends BaseActivity {
 
-    @BindView(R.id.iv)
-    AppCompatImageView iv;
+    @BindView(R.id.tv) TitanicTextView tv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,21 +25,17 @@ public class SplashActivity extends BaseActivity {
         setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
 
-        AnimatedVectorDrawableCompat ad = (AnimatedVectorDrawableCompat) iv.getDrawable();
+        tv.setTypeface(Typefaces.get(this, "Satisfy-Regular.ttf"));
 
-        ad.start();
-        ad.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
-            @Override
-            public void onAnimationStart(Drawable drawable) {
-                super.onAnimationStart(drawable);
-            }
+        final Titanic titanic = new Titanic();
+        titanic.start(tv);
 
-            @Override
-            public void onAnimationEnd(Drawable drawable) {
-                super.onAnimationEnd(drawable);
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                titanic.cancel();
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
-        });
+        }, 2000);
     }
 }
